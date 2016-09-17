cba3f7845394f1eb421238c05dfc4964aa0479dd
knadikari <nrv.rox.u@gmail.com>
2016-09-17 21:37:38 +0530

    git init repository

---
+++ app/src/main/java/com/ucsc/pnp/pettrack_data/LocationService.java
@@ -0,0 +1,50 @@
+package com.ucsc.pnp.pettrack_data;
+
+import android.app.Service;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by nrv on 9/17/16.
+ */
+public class LocationService extends Service implements LocationListener{
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        super.onStartCommand(intent, flags, startId);
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
